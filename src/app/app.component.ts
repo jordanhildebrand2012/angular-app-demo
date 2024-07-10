291b65c1b55a112ca51f289a30cd453d52e1ec3b
Jordan Shayo <jordanhildebrand2012@gmail.com>
2024-07-10 05:22:53 +0100

    manging input state without using signal state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
